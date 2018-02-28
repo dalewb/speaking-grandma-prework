91d92fe3372422038dbc48c8f30d16fb9b55c5b6
William Dale <williambdale@gmail.com>
2018-02-28 22:50:31 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -17,8 +17,6 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  # elsif phrase != phrase.upcase
-  #   return "HUH?! SPEAK UP, SONNY!"
   else 
     return "HUH?! SPEAK UP, SONNY!"
   end 
